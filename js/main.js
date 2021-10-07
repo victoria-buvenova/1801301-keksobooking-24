8fe3d25e72cf51e87aebb577c07fe080ee7da745
Victoria Buvenova <buvenova@gmail.com>
2021-10-08 00:26:31 +1300

    added a function to generate advertisement object

--- js/main.js
+++ js/main.js
@@ -89,11 +89,28 @@ function getOffer() {
     checkout: getRandomArrayElement(checkouts),
     features: features,
     description: 'Best house',
-    photos: Array(getRandomPositive()).fill(undefined).map(() => getRandomArrayElement(photos)),
+    photos: new Array(getRandomPositive()).fill(null).map(() => getRandomArrayElement(photos)),
   };
   return offer;
 }
 
+/**
+ *
+ * @returns объект advertisement, содержащий объекты author, offer and location
+ */
+function createAdvertisement() {
+  const author = getAuthor();
+  const offer = getOffer();
+  const location = getLocation();
 
-getAuthor();
-getOffer();
+  const advertisement = {
+    author: author,
+    offer: offer,
+    location: location,
+  };
+  return advertisement;
+}
+
+const objectGenerator = new Array(10).fill(null).map(createAdvertisement);
+
+objectGenerator;
