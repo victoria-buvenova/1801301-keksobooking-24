7024eb0048efb5ff2d8730dd258e6dbcaafaceb0
Victoria Buvenova <buvenova@gmail.com>
2021-10-16 22:41:33 +1300

    added mocks.js

---
+++ js/mocks.js
@@ -0,0 +1,13 @@
+const accommodationTypes = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
+const features = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const checkins = ['12:00', '13:00','14:00'];
+const checkouts = ['12:00', '13:00','14:00'];
+const photos = [
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
+];
+const descriptions = ['Best house', 'Cosy flat', 'Comfortable palace', 'Best price on the market', 'Real Japan'];
+const titles = ['Apartment for rent', 'House for rent', 'Palace for rent'];
+
+export {accommodationTypes, features, checkins, checkouts, photos,descriptions, titles};
