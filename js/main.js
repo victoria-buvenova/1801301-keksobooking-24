e4639937a7b8b8fdd257dc330a6029e911ad9258
Victoria Buvenova <buvenova@gmail.com>
2021-11-04 00:05:21 +1300

    подчистила main.js

--- js/main.js
+++ js/main.js
@@ -1,23 +1,4 @@
-import {getAuthor, getLocation, getOffer} from './data.js';
+import './data.js';
 import './card.js';
 import './validation-form.js';
-
-/**
- *
- * @returns объект advertisement, содержащий объекты author, offer and location
- */
-function createAdvertisement() {
-  const author = getAuthor();
-  const offer = getOffer();
-  const location = getLocation();
-
-  return {
-    author: author,
-    offer: offer,
-    location: location,
-  };
-}
-
-const objectGenerator = new Array(10).fill(null).map(createAdvertisement);
-
-objectGenerator;
+import './map.js';
