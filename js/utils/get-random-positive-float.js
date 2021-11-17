9523e700094226f64a983cbca4b82d761cea9d4c
Keks <keks@htmlacademy.ru>
2021-11-17 08:47:04 +0300

    :package:

--- js/utils/get-random-positive-float.js
+++ js/utils/get-random-positive-float.js
@@ -1,12 +1,12 @@
 // Функция взята из интернета и доработана
 // Источник - https://github.com/you-dont-need/You-Dont-Need-Lodash-Underscore#_random
 
-export function getRandomPositiveFloat (min, max, digits = 1) {
+function getRandomPositiveFloat (a, b, digits = 1) {
   // Чтобы не заставлять пользователя нашей функции помнить порядок аргументов,
   // реализуем поддержку передачи минимального и максимального значения в любом порядке,
   // а какое из них большее и меньшее вычислим с помощью Math.min и Math.max
-  const lower = Math.min(Math.abs(min), Math.abs(max));
-  const upper = Math.max(Math.abs(min), Math.abs(max));
+  const lower = Math.min(Math.abs(a), Math.abs(b));
+  const upper = Math.max(Math.abs(a), Math.abs(b));
   // Обратите внимание, чтобы учесть условие, что диапазон может быть [0, ∞),
   // мы не ругаем пользователя за переданное отрицательное число,
   // а просто берём его по модулю с помощью Math.abs
@@ -20,4 +20,3 @@ export function getRandomPositiveFloat (min, max, digits = 1) {
   // указать требуемое количество знаков после точки
   return result.toFixed(digits);
 }
-
